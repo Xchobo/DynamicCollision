85f188b405ea40566819775d4a369110a9cdcc61
Xchobo <xchobo@gmail.com>
2014-04-02 19:47:50 +0800

    Initial Commit

---
+++ DynamicCollision/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DynamicCollision
+//
+//  Created by Xchobo on 2014/4/2.
+//  Copyright (c) 2014年 Xchobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DynamicCollision/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DynamicCollision
+//
+//  Created by Xchobo on 2014/4/2.
+//  Copyright (c) 2014年 Xchobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
